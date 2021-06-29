01c675f65212c67ee651c8c758c56fb16a6da2a1
juan-rybczinski <jhreplay.lee@gmail.com>
2021-06-30 06:24:18 +0900

    Intial version of insurance PoC

--- main.py
+++ main.py
@@ -1,49 +1,46 @@
 import numpy as np
 from PIL import Image
 from feature_extractor import FeatureExtractor
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 from pathlib import Path
+from datetime import datetime
+
+import tensorflow as tf
 
 app = Flask(__name__)
 
 fe = FeatureExtractor()
+features = []
+img_paths = []
+for feature_path in Path("./static/feature").glob("*.npy"):
+    features.append(np.load(feature_path))
+    img_paths.append(Path("./static/img") / (feature_path.stem + ".jpg"))
+feature_list = np.array(features)
+
+try:
+    # Disable all GPUS
+    tf.config.set_visible_devices([], 'GPU')
+except:
+    pass
 
 
-@app.route('/api/image/search', methods=['POST'])
-def simple_image_search():
-    file = request.files['query_img']
-    tenant = request.form.get('tenant')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['query_img']
+        img = Image.open(file.stream)  # PIL image
+        uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + file.filename
+        img.save(uploaded_img_path)
 
-    feature_path = "static/" + tenant + "/features"
-    # Check and make tenant folder
-    Path(feature_path).mkdir(parents=True, exist_ok=True)
+        query = fe.extract(img)
+        dists = np.linalg.norm(feature_list - query, axis=1)  # L2 distances to features
+        ids = np.argsort(dists)[:2]  # Top 2 results
+        scores = [(dists[id], img_paths[id]) for id in ids]
 
-    query = fe.extract(Image.open(file.stream))
-
-    features = []
-    filenames = []
-    for feature in Path(feature_path).glob("*.npy"):
-        features.append(np.load(feature))
-        filenames.append(feature.stem)
-    features = np.array(features)
-
-    if len(features) == 0:
-        scores_tenant = []
+        return render_template('index.html', query_path=uploaded_img_path, results=scores)
     else:
-        # L2 distances to features
-        dists = np.linalg.norm(features - query, axis=1)
-        # ids = np.argsort(dists)[:5]  # Top 5 results
-        ids = np.argsort(dists)  # All results
-        scores_tenant = [
-            {"item_id": str(filenames[id]), "dist": str(dists[id])} for id in ids]
-
-    # Save feature
-    feature_path = feature_path + "/" + \
-                   (file.filename.split('.')[0] + ".npy")
-    np.save(feature_path, query)
-
-    return jsonify(scores_tenant)
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=9100)
+    app.run("0.0.0.0", port=8008)
